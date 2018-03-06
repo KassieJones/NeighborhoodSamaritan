ff8135d3abb94c7c775207cb8eb9ec359181ab74
KassondraJones <kassondralj@yahoo.com>
2018-03-06 16:00:20 -0500

    added config, model, user

--- src/com/gc/controller/HomeController.java
+++ src/com/gc/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.gc.controller;
 
+import javax.websocket.Session;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -33,6 +36,8 @@ public class HomeController {
 
 		return new ModelAndView("success", "message", message);
 	}
+	
+	
 	
 	@RequestMapping("/mainmenu")
 	public ModelAndView menuPage() {
---
+++ src/com/gc/model/User.java
@@ -0,0 +1,94 @@
+package com.gc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // marks the class as a hibernate entity (mapped class)
+@Table(name = "user") // maps the class to the DB table specified by the name modifier
+
+public class User {
+
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	
+	public User() {
+		
+	}
+
+	public User(String userName, String password, String firstName, String lastName, String email, String phone) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	@Id // maps the primary key
+	@Column(name = "Username") // name is optional if your column names match your variable names
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Column
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Column
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+
+}
